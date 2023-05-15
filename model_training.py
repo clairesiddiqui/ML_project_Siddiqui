98230995ed1e85ccae3a16922a4395d35181b376
Claire Siddiqui <csi@zmtnb0121.local>
2023-05-15 10:54:19 +0200

    installing packages

---
+++ model_training.py
@@ -0,0 +1,9 @@
+#================================================================#
+# Description:
+# This file is used to apply ML to the data collection based on
+# satellite and shipboard measurements from across the study 
+# region (Benguela Upwelling System, BUS)
+#================================================================#
+
+
+import pandas as pd
